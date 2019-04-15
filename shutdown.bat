ae20c693a54e861b96ccfd6148a37d3e3778f4da
Ashkan Rafiee <35492414+AshkanRafiee@users.noreply.github.com>
2019-04-15 19:19:14 +0430

    Update shutdown.bat

--- shutdown.bat
+++ shutdown.bat
@@ -2,13 +2,14 @@
 
 :Choice
 cls
+:Loop
 set /P q=Enter The Appropriate Hotkey(Shutdown="s",CancelShutdowns="c",Exit="e"):
-if /I "%q%" EQU "s" goto :Shut
-if /I "%q%" EQU "s" goto :Cancel
+if /I "%q%" EQU "s" goto :Shutcheck
+if /I "%q%" EQU "c" goto :Cancelcheck
 if /I "%q%" EQU "e" goto :Exit
 goto :Choice
 
-:Shut
+:Shutcheck
 cls
 set /P m=Enter Shutdown Time in Minutes:
 SET /A t=%m% * 60
@@ -16,16 +17,26 @@ SET /A t=%m% * 60
 ::echo %timee%
 :validation
 set /P l=Are You Sure You Want To Shutdown Your System in %t% Seconds?(y/n) or (ChangeMinutes=cm): 
-if /I "%l%" EQU "Y" echo shutdown -s -t %t%
+if /I "%l%" EQU "Y" goto :Shutdown
 if /I "%l%" EQU "N" goto :Choice
-if /I "%l%" EQU "CM" goto :Shut
+if /I "%l%" EQU "CM" goto :Shutcheck
 goto :validation
 
-:Cancel
+:Cancelcheck
 set /P l=Are You Sure You Want To Cancel Your Shutdown Queues?(y/n): 
-if /I "%l%" EQU "Y" echo shutdown -a
+if /I "%l%" EQU "Y" goto :Cancel
 if /I "%l%" EQU "N" goto :Choice
-goto Cancel
+goto :Cancelcheck
+
+:Shutdown
+shutdown.exe /s /t %t%
+echo your system will turn off in %t% seconds!
+goto :Loop
+
+:Cancel
+shutdown.exe /a
+echo your system will not turn off automatically!
+goto :Loop
 
 :Exit
 exit
